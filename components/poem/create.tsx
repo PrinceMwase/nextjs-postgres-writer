7e5993cdb2b19732fc56d5a0b6a337ee933bca8b
Prince Mwase <princefranklinemwase@gmail.com>
2023-09-05 09:41:34 +0200

    preview and confirmation

--- components/poem/create.tsx
+++ components/poem/create.tsx
@@ -5,52 +5,49 @@ import Line from "@/components/poem/line"
 // edit
 // post
 
+
+
 export default function CreatePoem() {
   const [content, setContent] = useState<string>();
+  const [preview, setPreview] = useState<boolean>(false);
+  const [confirm, setConfirmation] = useState<boolean>(false);
 
-  let myContentObject = {
-    "content" : content,
-    "conf":{
-        "text-alignment": "left",
-    },
-    "lines":{
-        "1":{
-            "bold":true,
-            "italic": false,
-            "color": null,
-        }
-    },
-  }
 
   return (
-    <form action="" method="post" className="bg-gray-50 px-4 py-8 sm:px-16 ">
+    <form action="" method="post" className="bg-gray-50 px-4 py-8 sm:px-16 flex-auto w-full" onSubmit={
+      (e)=>{
+        e.preventDefault();console.log(e); console.log(content);
+        
+    }}>
       <div>
         <label
           htmlFor="email"
           className="block text-xs text-gray-600 uppercase"
         >
-          Draft
+          Compose
         </label>
         <br />
-        <textarea
+        <div className="h-96 w-full">
+            {!preview && <textarea
           id="content"
           name="content"
-          placeholder="panic@thedis.co"
-          autoComplete="content"
-          rows={4}
+          placeholder="What's on your mind?"
           required
+          value={content}
           onChange={(e) => {setContent(e.currentTarget.value );}}
-          className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-        ></textarea>
-        <div>
-          {content?.split("\n")?.map(function (e: string, index) {
-
-            return <Line key={index} index={index} text={e}/>
+          className="mt-1 block w-full appearance-none rounded-md border h-full border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+        ></textarea>}
+        <div className="h-full w-full overflow-auto py-8">
+          {preview && content?.split("\n")?.map(function (e: string, index) {
+           
+            return <Line key={index} index={index} confirm={confirm} text={e}/>
 
           })}
         </div>
-        <button className="btn border-black border" disabled> Preview</button>
-        <button className="btn border-black border rounded-full" disabled> post </button>
+        </div>
+      
+        { !confirm && <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" onClick={()=>setPreview((value)=>!value)} type="button"> {preview ? "Edit" : "Preview"}</button>}
+        {preview && <button onClick={ ()=>{setConfirmation(true)} } className="h-10 px-6 font-semibold border rounded-md bg-white text-black" type={confirm ? "submit" : "button"} > {confirm ? "Post" : "Confirm?"} </button>}
       </div>
     </form>
   );
--- components/poem/line.tsx
+++ components/poem/line.tsx
@@ -1,10 +1,14 @@
 import { DragEvent, useState } from "react";
 
-export default function Line({ text, index }: { text: string; index: number }) {
+export default function Line({ text, index, confirm }: { text: string; index: number, confirm: boolean }) {
+
+  // line color
   const [color, setColor] = useState("#000000");
 
+  //line alignment
   const [align, setAlign] = useState<"left" | "right" | "center">("center");
 
+  //set alignment
   const handleAlign = (position: "left" | "right") => {
     if (align === "center" || align === position) {
       setAlign(position);
@@ -15,32 +19,32 @@ export default function Line({ text, index }: { text: string; index: number }) {
 
   return (
     <div>
-      <p style={{ color, textAlign: align }} className="text-lg font-medium font-zapf">
-        <span className="cursor-pointer" onClick={() => handleAlign("left")}>
+      <p style={{ color, textAlign: align }} className="text-lg font-medium font-zapf hover:translate-y-1 hover:translate-x-2 ">
+        {!confirm && <span className="cursor-pointer" onClick={() => handleAlign("left")}>
           {" "}
           &lt;{" "}
-        </span>
-        <label
+        </span>}
+        {!confirm && <label
           className="rounded-full border cursor-pointer font-sans"
           style={{ color, backgroundColor: color }}
-          htmlFor={"line" + index}
+          htmlFor={`${index}line${align}`}
         >
           {" "}
           ++
-        </label>
+        </label>}
         <input
           type="color"
-          id={"line" + index}
+          id={`${index}line${align}`}
           value={color}
-          name={"line" + index}
+          name={`${index}line${align}`}
           hidden
           onChange={(e) => setColor(e.currentTarget.value)}
         />{" "}
         {text}{" "}
-        <span className="cursor-pointer" onClick={() => handleAlign("right")}>
+        {!confirm && <span className="cursor-pointer" onClick={() => handleAlign("right")}>
           {" "}
           &gt;{" "}
-        </span>
+        </span>}
       </p>
     </div>
   );
