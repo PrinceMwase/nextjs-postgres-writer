7b0d36deb07318c5181c58ab4a7f37788fee9597
Prince Mwase <princefranklinemwase@gmail.com>
2023-10-14 23:56:32 +0200

    fixing types

--- types/profile.ts
+++ types/profile.ts
@@ -1,26 +1,31 @@
-type profile = {
+export type userDefinitions = {
   firstname: string | null;
   lastname: string | null;
   userTags: {
     tag: {
       tag: string
     };
-  }[];
-  writer: {
-    username: string;
-    about: string | null;
-    photo: {
-      link: string;
-    } | null;
-    Poem: ProfilePoemType[] | null;
-  }[];
+  }[] | null;
+}
+
+type Writer = {
+  username: string;
+  about: string | null;
+  photo: {
+    link: string;
+  } | null;
+  Poem: ProfilePoemType[] | null;
+};
+
+interface profile extends userDefinitions {
+  writer: Writer[];
 };
 export default profile;
 
 export type ProfilePoemType = {
   
     title: string;
-    description: string;
+    description: string | null;
     background: string;
     genre: {
       genre: string;
