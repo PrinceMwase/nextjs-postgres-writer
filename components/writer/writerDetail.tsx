a400b725fa2324ef533eb4a21e471189413bbf77
Prince Mwase <princefranklinemwase@gmail.com>
2023-10-19 19:35:48 +0200

    replaced with svg components

--- components/writer/writerDetail.tsx
+++ components/writer/writerDetail.tsx
@@ -3,19 +3,28 @@
 import Image from "next/image";
 import { useEffect } from "react";
 
-import { userDefinitions} from "types/profile";
+import { userDefinitions } from "types/profile";
+import IdCardIcon from "../svg/IdCardIcon";
+import InfoIcon from "../svg/InfoIcon";
+import HeartIcon from "../svg/HeartIcon";
+import NotificationIcon from "../svg/NotificationIcon";
 
 interface Props extends userDefinitions {
   pfp: string | undefined;
   about: string | null;
   username: string;
-  isProfile: boolean
+  isProfile: boolean;
 }
 
-export default function Details({username, userTags, about, firstname, lastname, pfp, isProfile}:Props) {
- 
-
-  
+export default function Details({
+  username,
+  userTags,
+  about,
+  firstname,
+  lastname,
+  pfp,
+  isProfile,
+}: Props) {
   return (
     <>
       {/* profile image  */}
@@ -31,113 +40,49 @@ export default function Details({username, userTags, about, firstname, lastname,
 
       {/* user details */}
       <div className="px-2 space-y-4">
-        <span className="font-bold block text-4xl font-maglite">{username}</span>
+        <span className="font-bold block text-4xl font-maglite">
+          {username}
+        </span>
 
         <div className="flex space-x-2">
           <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
-              />
-            </svg>
+            <IdCardIcon />
           </span>
-          <span className="font-medium">{firstname}{' '}{lastname}</span>
+          <span className="font-medium">
+            {firstname} {lastname}
+          </span>
         </div>
 
         <div className="flex space-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-            />
-          </svg>
+          <InfoIcon />
 
-          <span className="text-lg text-gray-600">
-            {about}
-          </span>
+          <span className="text-lg text-gray-600">{about}</span>
         </div>
 
         <div className="flex space-x-4">
-
-         
-          
-          {
-            userTags && userTags.map((value, key)=>{
-             return  <span className="flex" key={key}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
-              />
-            </svg>
-            {value.tag.tag}
-          </span>
-            })
-          }
-
+          {userTags &&
+            userTags.map((value, key) => {
+              return (
+                <span className="flex" key={key}>
+                  <HashTagIcon />
+                  {value.tag.tag}
+                </span>
+              );
+            })}
         </div>
       </div>
 
       {/* user icons */}
-     { !isProfile && <div className="flex space-x-4 py-4 px-2">
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
-            />
-          </svg>
-        </span>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-            />
-          </svg>
-        </span>
-      </div>}
+      {!isProfile && (
+        <div className="flex space-x-4 py-4 px-2">
+          <span>
+            <NotificationIcon />
+          </span>
+          <span>
+            <HeartIcon />
+          </span>
+        </div>
+      )}
     </>
   );
 }
