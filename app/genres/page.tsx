3e754f6b3325d2800bfe0fff66dc572a7e98907f
Prince Mwase <princefranklinemwase@gmail.com>
2023-10-19 20:02:05 +0200

    spacces on genres page

--- app/genres/page.tsx
+++ app/genres/page.tsx
@@ -13,20 +13,20 @@ export default async function Genres() {
       }
     })
   return (
-    <div className="p-10 h-screen">
+    <div className="py-10 px-4 h-screen">
       <div className="w-full mx-auto">
         <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
           {genres.map((genre, index) => (
-            <div key={index} className="">
+            <div key={index}>
               <Image
                 src={genre.photo.link}
                 alt={`${genre.genre} Thumbnail`}
-                className="mx-auto mb-4"
+                className="mx-auto mb-1"
                 width={100}
                 height={100}
                 style={{ maxWidth: "100%", height: "auto" }}
               />
-              <h2 className="text-md text-center capitalize font-semibold mb-2">{genre.genre}</h2>
+              <h2 className="text-sm text-center capitalize font-semibold mb-4">{genre.genre}</h2>
             </div>
           ))}
         </div>
