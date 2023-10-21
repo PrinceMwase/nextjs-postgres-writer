51e85ff645d900e443c4ac33b10bd0268a7014a1
Prince Mwase <princefranklinemwase@gmail.com>
2023-10-21 23:18:57 +0200

    complete revamp of the drawer

--- components/ui/Drawer.tsx
+++ components/ui/Drawer.tsx
@@ -1,61 +1,54 @@
-import InfoIcon from "@/components/svg/InfoIcon";
 import Link from "next/link";
 import SignOutDrawer from "./SignOutDrawer";
 import LikeIcon from "../svg/LikeIcon";
 import MuteIcon from "../svg/MuteIcon";
 import NotificationIcon from "../svg/NotificationIcon";
 import SettingsIcon from "../svg/SettingsIcon";
+import XIcon from "../svg/XIcon";
 export default function Drawer() {
   return (
     <div
       id="drawer"
       className="fixed inset-y-0 left-0 w-64 bg-white border-r  text-gray-950 py-4 transform transition-transform ease-in-out duration-300 -translate-x-full"
     >
-      <button
-        id="close-drawer-button"
-        className="bg-red-500 text-white p-2 rounded"
-      >
-        Close Drawer
-      </button>
+      <div className="w-full flex justify-between align-baseline">
+        <div className="px-4 text-xl"><span>Menu</span></div>
+        <button id="close-drawer-button" className="px-4">
+          <XIcon />
+        </button>
+      </div>
 
       <div className="py-10">
         <ul>
           <li className="border-b cursor-pointer hover:bg-slate-100">
             <div className="p-4 flex space-x-2 items-center">
-              <LikeIcon/>
+              <LikeIcon />
               <span className="font-semibold text-lg">Following</span>
             </div>
           </li>
           <li className="border-b cursor-pointer hover:bg-slate-100">
             <div className="p-4 flex space-x-2 items-center">
-              <MuteIcon/>
+              <MuteIcon />
               <span className="font-semibold text-lg">Muted</span>
             </div>
           </li>
           <li className="border-b cursor-pointer hover:bg-slate-100">
             <div className="p-4 flex space-x-2 items-center">
-              <NotificationIcon/>
+              <NotificationIcon />
               <span className="font-semibold text-lg">Notifications</span>
             </div>
           </li>
           <li className="border-b cursor-pointer hover:bg-slate-100">
             <Link href="/settings">
               <div className="p-4 flex space-x-2 items-center">
-                <SettingsIcon/>
+                <SettingsIcon />
                 <span className="font-semibold text-lg">
                   Settings & Privacy
                 </span>
               </div>
             </Link>
           </li>
-          <li className="border-b cursor-pointer hover:bg-slate-100">
-            <Link href="/about">
-              <div className="p-4 flex space-x-2 items-center">
-                <InfoIcon />{" "}
-                <span className="font-semibold text-lg">About Page</span>
-              </div>
-            </Link>
-          </li>
+
           <li className="border-b cursor-pointer hover:bg-slate-100">
             <SignOutDrawer />
           </li>
