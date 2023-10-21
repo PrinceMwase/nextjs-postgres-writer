e7b0913fca13142a3ec780e123bd4092a4b5bfa7
Prince Mwase <princefranklinemwase@gmail.com>
2023-10-21 23:25:00 +0200

    removed the extra event listeners

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
 "use client";
 import Create from "@/components/poem/create";
 import Infinite from "@/components/poem/infinite";
-import { useSession, getSession, SessionProvider } from "next-auth/react";
-import { useEffect } from "react";
+import {SessionProvider } from "next-auth/react";
 
 
 export default function Home() {
-  useEffect(() => {
-    // Add event listener to open drawer
-    const openDrawerButton = document.getElementById("open-drawer-button");
-    openDrawerButton?.addEventListener("click", () => {
-      const drawer = document.getElementById("drawer");
-      drawer?.classList.add("translate-x-0");
-    });
-
-    // Add event listener to close drawer
-    const closeDrawerButton = document.getElementById("close-drawer-button");
-    closeDrawerButton?.addEventListener("click", () => {
-      const drawer = document.getElementById("drawer");
-      drawer?.classList.remove("translate-x-0");
-    });
-  }, []);
 
   return (
     <>
       <SessionProvider>
         <Infinite>
-          <Create/>
+          <Create />
         </Infinite>
       </SessionProvider>
     </>
