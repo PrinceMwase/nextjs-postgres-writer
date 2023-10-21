68dbb185407b9cede3e043d85230b8031dfa875e
Prince Mwase <princefranklinemwase@gmail.com>
2023-10-21 23:24:36 +0200

    changed the app's title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-const title = "Next.js Prisma Postgres Auth Starter";
+const title = "Poet's Haven";
 const description =
-  "This is a Next.js starter kit that uses Next-Auth for simple email + password login and a Postgres database to persist the data.";
+  "This is an inclusive place for anyone who wants to jot down a poem";
 
 export const metadata: Metadata = {
   title,
